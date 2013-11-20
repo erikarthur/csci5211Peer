be10b860495b59a94a46d18dfa9771f750e39bfc
Erik Arthur <erikarthur@hotmail.com>
2013-11-20 14:01:12 -0600

    new getFiles

--- peer/getFile.cs
+++ peer/getFile.cs
@@ -4,12 +4,13 @@ using socketSrv;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.Text;
 
 namespace peer
 {
-	public class getFile
+	public class fileTransport
 	{
-		public void getFileFromNet (object data)
+		public void getFile (object data)
 		{
 			socketSrv.commandMessage cmd = (socketSrv.commandMessage)data;
 			IPEndPoint iep = new IPEndPoint(IPAddress.Any, cmd.port);
@@ -55,7 +56,7 @@ namespace peer
 			fileName = fileNameBytes.ToString();
 			int bytesLeft = (16 + fileNameSize + fileSize);
 			
-			if (numBytes == bytesLeft)
+			if (numBytes == bytesLeft)  //may need messageSize here
 			{
 				//got it all
 				//open fileStream and write the bytes
@@ -73,6 +74,90 @@ namespace peer
 			fileNetStream.Close();
 			tcpFIleNetClient.Close();
 		}
+		
+		public void sendFile (object data)
+		{
+			
+			
+			socketSrv.commandMessage cmd = (socketSrv.commandMessage)data;
+			
+			IPEndPoint iep = new IPEndPoint(cmd.peerIP, cmd.port);
+			TcpClient tcpSendFile = new TcpClient(iep);
+			NetworkStream netStream = tcpSendFile.GetStream();
+			
+			byte [] messageSizeBytes = new byte[4];
+			byte [] addressBytes = new byte[4];
+			byte [] portBytes = new byte[4];
+			byte [] cmdBytes = new byte[4];
+			byte [] fileSizeBytes = new byte[4];
+			byte [] fileNameSizeBytes = new byte[4];
+			byte [] fileNameBytes = new byte[255];
+			byte[] buffer = new byte[1500];
+			int messageSize, fileSize, fileNameSize, cmdNum, bufCnt;
+			string fileName;
+			
+			FileInfo fi1 = new FileInfo(cmd.fileName);
+			if (fi1.Exists)
+			{
+				fileSize = (int)fi1.Length;
+				fileName = fi1.Name;
+				fileNameSize = fileName.Length;
+			}
+			else
+				return;
+			
+			bufCnt = 0;
+			
+			messageSizeBytes = BitConverter.GetBytes(0);
+			System.Buffer.BlockCopy(buffer, 0, messageSizeBytes, 0, 4);
+			bufCnt += 4;
+
+			addressBytes = cmd.peerIP.GetAddressBytes();
+			System.Buffer.BlockCopy(buffer, bufCnt, addressBytes, 0, 4);
+			bufCnt += 4;
+
+			portBytes = BitConverter.GetBytes(cmd.port);
+			System.Buffer.BlockCopy(buffer, bufCnt, cmdBytes, 0, 4);
+			bufCnt += 4;
+			
+			cmdBytes = BitConverter.GetBytes(cmd.command);
+			System.Buffer.BlockCopy(buffer, bufCnt, cmdBytes, 0, 4);
+			bufCnt += 4;
+			
+			fileSizeBytes = BitConverter.GetBytes(fileSize);
+			System.Buffer.BlockCopy(buffer, bufCnt, fileSizeBytes, 0, 4);	
+			bufCnt += 4;
+				
+			UTF8Encoding encoder = new UTF8Encoding();
+			
+			fileNameSize = encoder.GetByteCount(fileName);
+				
+			fileNameSizeBytes = BitConverter.GetBytes(fileNameSize);
+			System.Buffer.BlockCopy(buffer, bufCnt, fileNameSizeBytes, 0, 4);
+			bufCnt += 4;
+			
+			fileNameBytes = encoder.GetBytes(fileName);
+			System.Buffer.BlockCopy(fileNameBytes, 0, buffer, bufCnt, fileNameSize);
+			bufCnt += fileNameSize;
+				
+			using (FileStream fs = File.OpenRead(cmd.fileName))
+	        {
+	            int readCnt = fs.Read(buffer,bufCnt,buffer.Length-bufCnt);
+	            while (readCnt > 0)
+	            {
+					bufCnt += readCnt;
+					messageSizeBytes = BitConverter.GetBytes(bufCnt);
+					System.Buffer.BlockCopy(messageSizeBytes, 0, buffer, 0, 4);
+	                //ready to send  - have bufCnt bytes in buffer
+					netStream.Write(buffer,0,bufCnt);
+					bufCnt = 0;
+					readCnt = fs.Read(buffer,bufCnt,buffer.Length);
+	            }
+	        }
+			
+			netStream.Close();
+			tcpSendFile.Close();
+			return;	
+		}
 	}
 }
-
